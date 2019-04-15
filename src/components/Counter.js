eb6cac4f12a2aa56a09279f9a633ec373928d05d
DwayneDevelops <mrsuccessfuldev@gmail.com>
2019-04-15 17:53:19 -0500

    stretch function incrementIfOdd breaks page

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,17 +6,19 @@ class Counter extends Component {
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+        if (this.props.count % 2 !== 0) {
+            this.state.increment();
+        }
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(() => this.state.increment(), 1000)
     };
 
     render() {
-        // Fill in the two button onClick methods
-        // Upon clicking these buttons, the count
-        // should decrement or increment accordingly
+        
         return (
             <p>
                 Clicked: {this.props.count} times
@@ -28,12 +30,12 @@ class Counter extends Component {
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button> 
             </p>
         );
     }
