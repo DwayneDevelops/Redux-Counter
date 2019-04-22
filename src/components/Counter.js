cad570c53dc96d9f3092dd3de4b07d4d6050df4d
DwayneDevelops <mrsuccessfuldev@gmail.com>
2019-04-22 16:05:44 -0500

    MVP

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,11 +5,14 @@ import { increment, decrement } from '../actions';
 class Counter extends Component {
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
+        if (this.props.count % 2 !== 0)
+        this.props.increment();
         // only increments if the counter value is odd
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
+        setTimeout(() => this.props.increment(), 1000)
         // increments after waiting for one second
     };
 
@@ -20,20 +23,22 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => {/* Fill me in */
+                this.props.increment() }}>
                     +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => {/* Fill me in */
+                this.props.decrement() }}>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button>  
             </p>
         );
     }
